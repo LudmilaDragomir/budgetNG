01d0a5102d4be4080b13f6d9dbb4efe1027d230b
Ludmila Dragomir <liuda.dragomir@gmail.com>
2019-08-28 15:56:47 +0100

    first commit of budget Angular practice project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { RecordsModule } from './records/records.module';
+import { SettingsModule } from './settings/settings.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    DashboardModule,
+    SharedModule,
+    AppRoutingModule,
+    RecordsModule,
+    SettingsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
